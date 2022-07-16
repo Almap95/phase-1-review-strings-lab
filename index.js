335705bb231364cc7e8adcf1702999bf4eb41590
Alma Perez <aperez32@student.scf.edu>
2022-07-15 23:50:08 -0400

    Assignment Done

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 
-let currentUser = 'Grace Hopper'
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
-const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!"
-const shortGreeting = "Welcome, " + currentUser.charAt(0) + "!" 
+//let currentUser = 'Grace Hopper'
+//const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+//const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!"
+//const shortGreeting = "Welcome, " + currentUser.charAt(0) + "!" 
+
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, " + ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
